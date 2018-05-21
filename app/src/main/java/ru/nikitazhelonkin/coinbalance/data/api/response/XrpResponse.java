9112f47d246740136a0a9e9546f853271132b7f0
Nikita Zhelonkin <niks.mbox@gmail.com>
2018-05-21 13:22:54 +0300

    Up version; XRP - fix display balance; Binance - fix display BCH balance

--- app/src/main/java/ru/nikitazhelonkin/coinbalance/data/api/response/XrpResponse.java
+++ app/src/main/java/ru/nikitazhelonkin/coinbalance/data/api/response/XrpResponse.java
@@ -1,16 +1,28 @@
 package ru.nikitazhelonkin.coinbalance.data.api.response;
 
 
+import android.support.annotation.NonNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+import ru.nikitazhelonkin.coinbalance.utils.ListUtils;
 
 public class XrpResponse {
 
-    @JsonProperty("account_data")
-    public AccountData accountData;
+    @JsonProperty("balances")
+    public List<Balance> mBalances;
 
-    public static class AccountData {
+    public Balance getBalance(@NonNull String currency){
+        return ListUtils.find(mBalances, b -> currency.equalsIgnoreCase(b.currency));
+    }
 
-        @JsonProperty("initial_balance")
+    public static class Balance {
+
+        @JsonProperty("currency")
+        public String currency;
+        @JsonProperty("value")
         public String balance;
 
     }
